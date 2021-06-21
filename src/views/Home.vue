387db8be7d146cd87fe25d35ae7a0bb3ee566860
Veronica <veronica.alcala@gmail.com>
2021-06-21 12:06:38 +0200

    using refs for reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>home</h1>
+    <p ref="p">{{name}} and {{age}}</p>
+    <button @click="handleClick">click me</button>
+    <button @click="age++">Increase age</button>
+    <input type="text" v-model="name">
   </div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from '@vue/reactivity'
+
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+
+    // console.log(this)
+
+    // const p = ref(null)
+
+    let name = ref('mario')
+    let age = ref(30)
+
+    const handleClick = () => {
+      name.value = 'luigi'
+      age.value = 33
+      // console.log(p, p.value)
+      // p.value.classList.add('test')
+      // p.value.textContent = 'hello, ninjas'
+    }
+
+    return { name, age, handleClick }
   }
 }
 </script>
